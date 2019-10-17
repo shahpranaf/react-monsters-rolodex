2c3880e1f1a757bfb588d308a01d996908c7b976
shahpranaf <shahpranaf@gmail.com>
2019-10-17 11:58:36 +0530

    Updates

---
+++ precache-manifest.1a339da02f820a7eb15341166000bf73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b82b734050b4e12feb84771ed35d045f",
+    "url": "/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "17594a3a48d58173f379",
+    "url": "/react-monsters-rolodex/static/css/main.b313d09b.chunk.css"
+  },
+  {
+    "revision": "f977fa80ff73af86627e",
+    "url": "/react-monsters-rolodex/static/js/2.37e904de.chunk.js"
+  },
+  {
+    "revision": "17594a3a48d58173f379",
+    "url": "/react-monsters-rolodex/static/js/main.187c5df9.chunk.js"
+  },
+  {
+    "revision": "e3713dfbf0885b83451f",
+    "url": "/react-monsters-rolodex/static/js/runtime-main.f9329b93.js"
+  }
+]);
